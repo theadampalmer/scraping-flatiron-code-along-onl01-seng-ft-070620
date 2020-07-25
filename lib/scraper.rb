54f95ebe955e734eed11a1dbc7ac1e7b62df261d
Adam Palmer <theadampalmer@gmail.com>
2020-07-25 17:34:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,23 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-
+ 
 require_relative './course.rb'
-
+ 
 class Scraper
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-  
+ 
   def get_page
+ 
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
  
     #doc.css(".post").each do |post|
@@ -27,7 +17,6 @@ class Scraper
       #course.description = post.css("p").text
     #end
   end
+ 
 end
-
-Scraper.new.get_page
 
